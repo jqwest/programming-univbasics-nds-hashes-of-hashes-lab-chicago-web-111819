a50a2eaca47f1d87d5fe33323b8d1f8bd8625ae6
john L <johnleavell@gmail.com>
2019-11-03 02:22:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,8 +32,21 @@
 
 def naming_system
   # Remember:
-  kingdom = {},
-  
+  kingdom = {
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species",
+                sub_category: nil } } } } } } },
+
   #  Kingdom
   #  Phylum
   #  Class
